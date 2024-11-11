ec1bbc7e6589084c253c272f9de3ce21c309fd6a
Zimmer <magnuszimmer11@gmail.com>
2024-11-11 09:55:53 +0100

    UserData files

---
+++ UserData.c
@@ -0,0 +1,5 @@
+//
+// Created by magnu on 11/11/2024.
+//
+
+#include "UserData.h"
---
+++ UserData.h
@@ -0,0 +1,8 @@
+//
+// Created by magnu on 11/11/2024.
+//
+
+#ifndef USERDATA_H
+#define USERDATA_H
+
+#endif //USERDATA_H
